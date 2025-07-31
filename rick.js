26442fb2c1ffa04ff5c4fe509f28746581e6ea94
Glibn <romanuting@gmail.com>
2025-07-31 20:42:10 +0300

    d

--- rick.js
+++ rick.js
@@ -1,61 +1,28 @@
-//1
-const input = document.createElement("input");
-input.type = "text";
-input.placeholder = "Введіть текст";
-input.id = "inputText";
-
-const button = document.createElement("button");
-button.id = "myButton";
-button.textContent = "Натисни мене";
-
-document.body.append(input, button);
-
+// 1.
+const button =
+  document.getElementById("myButton");
+const input =
+  document.getElementById("textInput");
 button.addEventListener("click", () => {
   button.textContent = input.value;
 });
-//2
-const title = document.createElement("h2");
-title.textContent = "Моє зображення";
 
-const img = document.createElement("img");
-img.src = "old.jpg";
-img.width = 200;
+// 2.
+const image = document.getElementById("myImage");
+image.src = "./unnamed.png";
 
-document.body.append(title, img);
-
-img.src = "new.jpg";
-//3
-const link = document.createElement("a");
-link.id = "myLink";
-link.href =
-  "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
-link.textContent = "Перейти";
-
-const linkedImg = document.createElement("img");
-linkedImg.src = "image.jpg";
-linkedImg.width = 200;
-
-linkedImg.alt = "зображення";
-
-document.body.append(
-  link,
-  document.createElement("br"),
-  img
+// 3.
+const link = document.getElementById("myLink");
+const linkImg =
+  document.getElementById("linkImage");
+link.href = "https://new-url.com";
+linkImg.setAttribute(
+  "alt",
+  "Опис нового зображення"
 );
 
-link.href = "https://new-site.com";
-img.alt = "не зображення";
-//4
-const ul = document.createElement("ul");
-ul.id = "myList";
-
-const li1 = document.createElement("li");
-li1.textContent = "Старий текст 1";
-
-const li2 = document.createElement("li");
-li2.textContent = "Старий текст 2";
-
-ul.append(li1, li2);
-document.body.append(ul);
-
-ul.firstElementChild.textContent = "Новий текст ";
+// 4.
+const list = document.getElementById("myList");
+const firstItem = list.querySelector("li");
+firstItem.textContent =
+  "Новий текст першого елемента";
